ab22b97ffc82b2ffd0909fa8097c4851b1c14cfe
Zhaofeng Li <hello@zhaofeng.li>
2022-08-28 18:22:10 -0600

    steam-jupiter-original: Use wildcard to match fat bootstrap tarball

--- pkgs/steam-jupiter-original/default.nix
+++ pkgs/steam-jupiter-original/default.nix
@@ -6,8 +6,6 @@
 { steam-original, fetchurl }:
 
 let
-  fatBootstrap = "steam_jupiter_stable_bootstrapped_20220802.1.tar.xz";
-
   bundle = fetchurl {
     url = "https://steamdeck-packages.steamos.cloud/archlinux-mirror/sources/jupiter-main/steam-jupiter-stable-1.0.0.74-2.19.src.tar.gz";
     sha256 = "sha256-jY5gPTHQ8oLg2nqd2yNQWjVGuX4fl7uMPCpphJV4EH0=";
@@ -17,7 +15,7 @@ in steam-original.overrideAttrs (old: {
   pname = "steam-jupiter-original";
   postInstall = (old.postInstall or "") + ''
     >&2 echo ":: Injecting Steam Deck client bootstrap..."
-    tar xvf ${bundle} steam-jupiter-stable/${fatBootstrap} --strip-components=1
-    cp ${fatBootstrap} $out/lib/steam/bootstraplinux_ubuntu12_32.tar.xz
+    tar xvf ${bundle}
+    cp steam-jupiter-stable/steam_jupiter_stable_bootstrapped_*.tar.xz $out/lib/steam/bootstraplinux_ubuntu12_32.tar.xz
   '';
 })
