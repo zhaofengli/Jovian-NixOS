1bed8cbeb04487be9ff9699b11b57586ce796aa7
Zhaofeng Li <hello@zhaofeng.li>
2022-11-06 23:55:00 -0700

    steamdeck/sdgyrodsu: Move udev rule inside module, use input group

--- modules/steamdeck/sdgyrodsu.nix
+++ modules/steamdeck/sdgyrodsu.nix
@@ -24,24 +24,23 @@ in
     };
   };
   config = mkIf cfg.enableGyroDsuService {
-    assertions = [
-      {
-        assertion = cfg.enableGyroDsuService -> cfg.enableControllerUdevRules;
-        message = "jovian.devices.steamdeck.enableControllerUdevRules must be enabled for enableGyroDsuService";
-      }
-    ];
-
     systemd.services.sdgyrodsu = {
       description = "Cemuhook DSU server for the Steam Deck Gyroscope";
       wantedBy = [ "multi-user.target" ];
       path = [ pkgs.usbutils ];
       serviceConfig = {
         DynamicUser = true;
+        SupplementaryGroups = [ "input" ];
         ExecStart = "${pkgs.sdgyrodsu}/bin/sdgyrodsu";
         PrivateTmp = true;
         ProtectSystem = "strict";
         ProtectHome = true;
       };
     };
+
+    services.udev.extraRules = ''
+      # This rule is needed to expose the hiddev devices to sdgyrodsu
+      SUBSYSTEM=="usbmisc", ATTRS{idVendor}=="28de", MODE="0660", GROUP="input"
+    '';
   };
 }
