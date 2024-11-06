84da0883c26098a343b3056c025943d991d1c7f5
K900 <me@0upti.me>
2024-11-06 09:09:13 +0300

    decky-loader: 3.0.1 -> 3.0.4

--- pkgs/decky-loader/default.nix
+++ pkgs/decky-loader/default.nix
@@ -8,19 +8,19 @@
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "decky-loader";
-  version = "3.0.1";
+  version = "3.0.4";
 
   src = fetchFromGitHub {
     owner = "SteamDeckHomebrew";
     repo = "decky-loader";
     rev = "v${version}";
-    hash = "sha256-IFop3SbXYCzYdHGRK08v8uPL4sSAHelCrIEc8HquZmg=";
+    hash = "sha256-pWkAu0nYg3YOA7w/8eN9n23sSyFkZcuvGUF8Swd0Hbc=";
   };
 
   pnpmDeps = pnpm.fetchDeps {
     inherit pname version src;
     sourceRoot = "${src.name}/frontend";
-    hash = "sha256-iQjWuD0NhWiGdx6L7sDdHqdHSbkOjqa/0wer1Revvc8=";
+    hash = "sha256-l4AA3xOdouk08i9n0lWbzeKCTqEkXC0BOsW1uxQMPyo=";
   };
 
   pyproject = true;
--- pkgs/decky-loader/prerelease.nix
+++ pkgs/decky-loader/prerelease.nix
@@ -1,22 +1,4 @@
 { 
   decky-loader,
-  fetchFromGitHub,
-  pnpm,
 }:
-decky-loader.overridePythonAttrs rec {
-  pname = "decky-loader";
-  version = "3.0.4-pre1";
-
-  src = fetchFromGitHub {
-    owner = "SteamDeckHomebrew";
-    repo = "decky-loader";
-    rev = "v${version}";
-    hash = "sha256-pWkAu0nYg3YOA7w/8eN9n23sSyFkZcuvGUF8Swd0Hbc=";
-  };
-
-  pnpmDeps = pnpm.fetchDeps {
-    inherit pname version src;
-    sourceRoot = "${src.name}/frontend";
-    hash = "sha256-l4AA3xOdouk08i9n0lWbzeKCTqEkXC0BOsW1uxQMPyo=";
-  };
-}
+decky-loader
