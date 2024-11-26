1bf1ab65826acacef4a467d735a2388639862fc5
K900 <me@0upti.me>
2024-11-26 10:23:31 +0300

    jupiter-hw-support: 20240919.2 -> 20241119.1

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "jupiter-hw-support-source";
-  version = "20240919.2";
+  version = "20241119.1";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "jupiter-hw-support";
     rev = "jupiter-${version}";
-    hash = "sha256-K3NhBWiKObi4JVg28r+rqanjZGkHTC2QL/u09B7sRA8=";
+    hash = "sha256-eoFg39ehgM1Wfo5WOXWA9WOIZX5ZH2TzoqZhkFOgLSE=";
   };
 
   patches = [
@@ -23,8 +23,6 @@ stdenv.mkDerivation rec {
       systemd = systemd;
       src = ./automount-fix-system-paths.patch;
     })
-    # Fix controller updates with python-hid >= 1.0.6
-    ./hid-1.0.6.patch
     # Remove `deck` username assumption
     ./0001-Jovian-Ensure-automounting-works-for-any-UID-1000-us.patch
     # Minor fixes against silly environments
